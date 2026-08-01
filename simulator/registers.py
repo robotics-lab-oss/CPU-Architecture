41b7a56fbd72f155a47016ed8dcb0d7863e3a4bb
Robotics <rv851304@gmail.com>
2026-08-01 21:15:26 +0530

    Implement Registers class with reset and dump methods

---
+++ simulator/registers.py
@@ -0,0 +1,30 @@
+class Registers:
+
+    def __init__(self):
+        self.reset()
+
+
+    def reset(self):
+
+        self.A = 0       # Accumulator
+        self.B = 0       # General Register
+
+        self.PC = 0      # Program Counter
+        self.IR = 0      # Instruction Register
+
+        self.Z = 0       # Zero Flag
+        self.C = 0       # Carry Flag
+        self.N = 0       # Negative Flag
+
+
+    def dump(self):
+
+        print("----------------")
+        print(f"A   : {self.A:02X}")
+        print(f"B   : {self.B:02X}")
+        print(f"PC  : {self.PC:02X}")
+        print(f"IR  : {self.IR:02X}")
+        print(
+            f"FLAGS Z:{self.Z} C:{self.C} N:{self.N}"
+        )
+        print("----------------")
