cc097510a6a3ee46742e1073321c518f3d986e9f
Robotics <rv851304@gmail.com>
2026-08-01 19:39:49 +0530

    Refactor assembler functionality into Assembler class

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,32 +1,107 @@
+# assembler.py
+
 from opcode import OPCODES
+from lexer import tokenize
+from parser import parse
+from symbol_table import SymbolTable
 
-def assemble(line):
-    parts = line.strip().split()
 
-    if len(parts) == 0:
-        return None
+class Assembler:
 
-    instruction = parts[0].upper()
+    def __init__(self):
+        self.symbols = SymbolTable()
+        self.machine_code = []
 
-    opcode = OPCODES[instruction]
+    def first_pass(self, source):
 
-    operand = 0
+        address = 0
 
-    if len(parts) > 1:
-        operand = int(parts[1].replace("#", ""))
+        for line in source:
 
-    machine = (opcode << 4) | operand
+            line = line.strip()
 
-    return machine
+            if not line:
+                continue
 
-program = [
-    "LOAD #5",
-    "ADD #3",
-    "HALT"
-]
+            tokens = tokenize(line)
 
-for line in program:
-    code = assemble(line)
+            if not tokens:
+                continue
 
-    if code is not None:
-        print(f"{line:10} -> {code:08b}")
+            if tokens[0][0] == "LABEL":
+                self.symbols.add(tokens[0][1], address)
+                continue
+
+            address += 1
+
+    def second_pass(self, source):
+
+        for line in source:
+
+            line = line.strip()
+
+            if not line:
+                continue
+
+            tokens = tokenize(line)
+
+            if not tokens:
+                continue
+
+            if tokens[0][0] == "LABEL":
+                continue
+
+            inst = parse(line)
+
+            opcode = OPCODES[inst["opcode"]]
+
+            operand = 0
+
+            if inst["operand"] is not None:
+
+                if inst["operand_type"] == "IMMEDIATE":
+                    operand = int(inst["operand"])
+
+                elif inst["operand_type"] == "ADDRESS":
+                    operand = int(inst["operand"],16)
+
+                elif inst["operand_type"] == "REGISTER":
+                    operand = int(inst["operand"][1])
+
+            machine = (opcode << 4) | operand
+
+            self.machine_code.append(machine)
+
+    def dump(self):
+
+        print("Machine Code")
+
+        print("----------------")
+
+        for code in self.machine_code:
+            print(f"{code:08b}")
+
+
+if __name__ == "__main__":
+
+    source = [
+
+        "START:",
+        "LOAD #5",
+        "ADD #3",
+        "STORE 0x20",
+        "HALT"
+
+    ]
+
+    asm = Assembler()
+
+    asm.first_pass(source)
+
+    asm.second_pass(source)
+
+    asm.symbols.dump()
+
+    print()
+
+    asm.dump()
